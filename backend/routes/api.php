7eb9481a4b108a119ebdab0bbd507a4209f342f7
Pradip Baniya <pradipbaniya422@gmail.com>
2026-06-19 15:36:37 +0545

    Implement blogs backend API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AboutFeatureController;
 use App\Http\Controllers\Api\AboutPageController;
 use App\Http\Controllers\Api\Admin\SiteSettingController as AdminSiteSettingController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BlogController;
 use App\Http\Controllers\Api\ContactInquiryController;
 use App\Http\Controllers\Api\SiteSettingController;
 use Illuminate\Http\Request;
@@ -12,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/site-settings', [SiteSettingController::class, 'index']);
 
 Route::post('/admin/site-settings', [AdminSiteSettingController::class, 'update']);
+
+Route::get('/blogs', [BlogController::class, 'publicIndex']);
+Route::get('/blogs/{slug}', [BlogController::class, 'show']);
+Route::get('/admin/blogs', [BlogController::class, 'index']);
+Route::post('/admin/blogs', [BlogController::class, 'store']);
+Route::put('/admin/blogs/{blog}', [BlogController::class, 'update']);
+Route::delete('/admin/blogs/{blog}', [BlogController::class, 'destroy']);
 
 Route::get('/about-page', [AboutPageController::class, 'index']);
 Route::post('/admin/about-page', [AboutPageController::class, 'update']);
