41ac7f1a6f53fab761a9e7b136c5646f712b27a2
frommanbir <your.email@example.com>
2026-07-07 14:43:16 +0545

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,12 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    dangerouslyAllowLocalIP: true, //Remove in development
     remotePatterns: [
       {
         protocol: 'http',
         hostname: '127.0.0.1',
         port:'8000',
         pathname:'/storage/**',
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/storage/**",
       },
       {
         protocol: 'http',
