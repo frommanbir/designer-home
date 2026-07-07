d4edbf33d616bb867ad0ac887a0cfa6a1dfeb0cb
frommanbir <your.email@example.com>
2026-07-07 11:46:23 +0545

    confignext.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port:'8000',
+        pathname:'/storage/**',
+      },
+      {
+        protocol:'https',
+        hostname:'194.146.12.71',
+        port:'8020',
+        pathname:'/storage/**'
+      },
+    ],
+  },
 };
 
 export default nextConfig;
