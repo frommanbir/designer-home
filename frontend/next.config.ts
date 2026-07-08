78b0bf61f86c596ed60d63fc70809f0518471aa3
frommanbir <your.email@example.com>
2026-07-08 07:06:29 +0545

    changesadded sharp for image optimization

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    minimumCacheTTL: 31536000, // 1 year cache TTL for optimized images in production
     dangerouslyAllowLocalIP: true, //Remove in development
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920], // Limit the viewport widths Next.js optimizes for
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // Limit standard inline image sizes
     remotePatterns: [
       {
         protocol: 'http',
